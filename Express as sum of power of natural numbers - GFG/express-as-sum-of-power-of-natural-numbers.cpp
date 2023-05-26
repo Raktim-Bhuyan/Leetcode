8b7d20d029b7531294037c6ff8f3ec03d5bf1bee
Raktim Bhuyan <rokuongo@gmail.com>
2023-05-26 21:41:29 +0530

    Added solution - LeetHub

--- Express as sum of power of natural numbers - GFG/express-as-sum-of-power-of-natural-numbers.cpp
+++ Express as sum of power of natural numbers - GFG/express-as-sum-of-power-of-natural-numbers.cpp
@@ -7,21 +7,21 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
-#define ll long long
-#define M 1000000007
+
 class Solution{
     public:
+    int M = 1e9 + 7;
     int dp[1001][1001];
-    int solve(int i,vector<int> &v,int tot,int x){
+    
+    int solve(int i,vector<int> &v,int tot){
         if(tot<0)return 0;
         if(i==v.size()){
             return 0==tot;
         }
-        if(dp[i][tot]==-1){
-            dp[i][tot] = solve(i+1,v,tot-v[i],x)%M+solve(i+1,v,tot,x)%M;
-            dp[i][tot] %=M;
-        }
-        return dp[i][tot];
+        if(dp[i][tot]!=-1)return dp[i][tot];
+       
+         return  dp[i][tot] = (solve(i+1,v,tot-v[i])%M+solve(i+1,v,tot)%M)%M;
+      
             
     }
     int numOfWays(int n, int x)
@@ -32,7 +32,7 @@ class Solution{
             v.push_back(pow(i,x));
         }
         
-        return solve(0,v,n,n);
+        return solve(0,v,n);
     }
 };
 
